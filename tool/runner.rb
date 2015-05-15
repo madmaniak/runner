ed8246c590108dccc1170560bbc9f1d6f387ded0
Marek Piasecki <mad@maniak.pro>
2015-05-15 15:01:45 -0500

    little ref with ostruct; format output

--- tool/runner.rb
+++ tool/runner.rb
@@ -1,11 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'ostruct'
+require 'colorize'
 
 class Runner
-
-  TYPE = 0
-  DESCRIPTION = 1
-  CODE = 2
 
   class << self
 
@@ -33,20 +31,20 @@ class Runner
 
     def run
       walk ->(line){
-        puts line[DESCRIPTION]
-        line[CODE].call if line[TYPE] == :step
+        puts decorate(line, :run)
+        line.code.call if line.type == :step
       }
     end
 
-    def documentation
+    def doc
       walk ->(line){
       }
     end
 
     private
 
-    def new_line(*structure)
-      current_task << structure # type, description, code/subtasks
+    def new_line(type, description, code = nil)
+      current_task << OpenStruct.new(type: type, description: description, code: code)
     end
 
     def current_task
@@ -56,7 +54,27 @@ class Runner
     def walk(job, list = @document)
       list.each do |line|
         job.call(line)
-        walk(job, line[CODE]) if line[CODE].is_a? Array
+        walk(job, line.code) if line.code.is_a? Array
+      end
+    end
+
+    def decorate(line, mode)
+      description = line.description
+
+      if mode == :run
+        case line.type
+        when :title then description.white.on_blue
+        when :task then description.light_blue
+        when :description then description.magenta
+        when :step then description.light_magenta
+        end
+      elsif mode == :doc
+        case line.type
+        when :title then "# #{description}"
+        when :task then "## #{description}"
+        when :description then "_#{description}_"
+        when :step then description
+        end
       end
     end
 
